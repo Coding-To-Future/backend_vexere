00c94a1071b5489ce9e4e7d7b51c79dc9800b0fe
ironhight <conanstark2468@gmail.com>
2020-04-25 20:07:44 +0700

    test alias

--- models/Ticket.js
+++ models/Ticket.js
@@ -3,26 +3,26 @@ const mongoose = require('mongoose');
 const { seatSchema } = require('./Seat');
 
 const ticketSchema = new mongoose.Schema({
-  tripId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'Trip',
-  },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User',
-  },
-  seats: [seatSchema],
-  totalPrice: {
-    type: Number,
-    required: true,
-    validate(value) {
-      if (value < 0) {
-        throw new Error('Price must be a positive number');
-      }
+    tripId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Trip',
     },
-  },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+    },
+    seats: [seatSchema],
+    totalPrice: {
+        type: Number,
+        required: true,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Price must be a positive number');
+            }
+        },
+    },
 });
 
 const Ticket = mongoose.model('Ticket', ticketSchema, 'Ticket');
