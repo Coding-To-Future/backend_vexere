224c7e2cd5eb1e99794d68c12885491f1f81ab2b
ironhight <mrnguyenhoainam2468@gmail.com>
2020-08-17 00:21:17 +0700

    alert after login

--- models/User.js
+++ models/User.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const moment = require('moment');
-const validator = require('validator');
-const jwt = require('jsonwebtoken');
-const Ticket = require('../models/Ticket');
-const keys = require('../config/index');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const moment = require("moment");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const Ticket = require("../models/Ticket");
+const keys = require("../config/index");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
     lowercase: true,
     validate(value) {
       if (!validator.isEmail(value)) {
-        throw new Error('Email is invalid');
+        throw new Error("Email is invalid");
       }
     },
   },
@@ -25,23 +25,23 @@ const userSchema = new mongoose.Schema({
     trim: true,
     minlength: 6,
     validate(value) {
-      if (value.toLowerCase().includes('password')) {
+      if (value.toLowerCase().includes("password")) {
         throw new Error('Password cannot contain "password"');
       }
     },
   },
   fullName: { type: String, required: true, trim: true },
-  userType: { type: String, default: 'client' },
+  userType: { type: String, default: "client" },
   phoneNumber: { type: String, required: true, trim: true, maxlength: 10 },
   dayOfBirth: { type: String, required: true, trim: true },
   avatar: { type: String, trim: true },
   tokens: [{ token: { type: String, required: true } }],
 });
 
-userSchema.virtual('ticket', {
-  ref: 'Ticket',
-  localField: '_id',
-  foreignField: 'userId',
+userSchema.virtual("ticket", {
+  ref: "Ticket",
+  localField: "_id",
+  foreignField: "userId",
 });
 
 userSchema.methods.toJSON = function () {
@@ -57,7 +57,7 @@ userSchema.methods.toJSON = function () {
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
   const token = jwt.sign(
-    { _id: user._id, userType: user.userType },
+    { _id: user._id, userType: user.userType, fullName: user.fullName },
     keys.secret_key,
     {
       expiresIn: 3600,
@@ -71,32 +71,32 @@ userSchema.methods.generateAuthToken = async function () {
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email });
   if (!user) {
-    throw new Error('Email không tồn tại trong hệ thống');
+    throw new Error("Email không tồn tại trong hệ thống");
   }
   const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch) {
-    throw new Error('Mật khẩu không chính xác');
+    throw new Error("Mật khẩu không chính xác");
   }
   return user;
 };
 
-userSchema.pre('save', async function (next) {
+userSchema.pre("save", async function (next) {
   const user = this;
-  if (user.isModified('password')) {
+  if (user.isModified("password")) {
     user.password = await bcrypt.hash(user.password, 10);
   }
-  if (user.isModified('dayOfBirth')) {
-    user.dayOfBirth = moment(user.dayOfBirth).format('DD/MM/YYYY');
+  if (user.isModified("dayOfBirth")) {
+    user.dayOfBirth = moment(user.dayOfBirth).format("DD/MM/YYYY");
   }
   next();
 });
 
-userSchema.pre('remove', async function (next) {
+userSchema.pre("remove", async function (next) {
   const user = this;
   await Ticket.deleteMany({ userId: user._id });
   next();
 });
 
-const User = mongoose.model('User', userSchema, 'User');
+const User = mongoose.model("User", userSchema, "User");
 
 module.exports = User;
